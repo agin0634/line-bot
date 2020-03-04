77d7f796ceba0f70a2dd942e8e4f6b5f71f19661
Duri Shen <agin0511@Duris-MacBook-Air.local>
2020-03-04 01:21:25 -0800

    using Cloud Storage

--- imagebot/imagebot.go
+++ imagebot/imagebot.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	bucket := "line-bot-01-269403.appspot.com"
+
 	bot, err := linebot.New(
 		os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"),
 	)
@@ -31,6 +43,21 @@ func main() {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
+					object := message.Text + ".jpg"
+
+					ctx, cancel := context.WithTimeout(ctx, time.Second*10)
+					defer cancel()
+					obj := client.Bucket(bucket).Object(object)
+					if _, err := obj.Attrs(ctx); err != nil {
+						if _, err = bot.ReplyMessage(
+							event.ReplyToken,
+							linebot.NewTextMessage("錯誤，請重新上傳")).Do(); err != nil {
+							log.Fatal(err)
+						}
+						log.Fatalf("Message: %v", err)
+						return
+					}
+
 					originalContentURL := "https://storage.googleapis.com/line-bot-01-269403.appspot.com/" + message.Text + ".jpg"
 					previewImageURL := "https://storage.googleapis.com/line-bot-01-269403.appspot.com/" + message.Text + ".jpg"
 
